5a53b14d089939c81fbae3547ab36871f6479f08
JiHyeonBeak <jadehl09@gmail.com>
2024-05-08 10:38:57 +0900

    update greeting

--- Sub.py
+++ Sub.py
@@ -1,13 +1,16 @@
 from flask import *
+import datetime as dt
 
 app = Flask(__name__)
 
+@app.route('/')
+def call_sub():
+    now = process_date()
+    return render_template('sub.html',now=now)
+
+def process_date():
+    return dt.datetime.now()
+
+# 최종 웹서버 실행 함수
 if __name__ == '__main__':
     app.run()
-
-@app.route('/test')
-def call_sub():
-    return render_template('sub.html')
-
-def process_data():
-    return
