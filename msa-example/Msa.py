31fa2d6eb104d8e9f2185c6af2964d22363ef7e6
JiHyeonBeak <jadehl09@gmail.com>
2024-05-10 09:09:37 +0900

    add input

--- msa-example/Msa.py
+++ msa-example/Msa.py
@@ -6,11 +6,15 @@ app = Flask(__name__)
 @app.route('/')
 def call_sub():
     now = process_date()
-    return render_template('sub.html',now=now)
+    return render_template('main.html',now=now)
 
 def process_date():
     return dt.datetime.now()
 
-# 최종 웹서버 실행 함수
+@app.route("/shootingdata", methods=['POST'])
+def add_data():
+    data = request.form.get("wrapData")
+    return render_template('main.html', rdata=data)
+
 if __name__ == '__main__':
     app.run()
