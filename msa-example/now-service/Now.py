c95ca19a19129817ac431793f9ae6cb50eb3e6ff
jh <jadehl09@gmail.com>
2024-05-16 16:39:18 +0900

    add data api

--- msa-example/now-service/Now.py
+++ msa-example/now-service/Now.py
@@ -2,5 +2,14 @@ from flask import *
 import datetime as dt
 import sqlite3
 
+app = Flask(__name__)
+
+# 메모리DB connection 설정.
+con = sqlite3.connect(':memory:',check_same_thread=False)
+
+@app.route('/wtisit')
+def process_date():
+    return dt.datetime.now()
+
 if __name__ == '__main__':
     app.run()
